69802bc57ed71f95f270d40abefd51d4e08b7ab6
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-20 09:53:38 -0600

    Started Hello World

---
+++ Hello World/helloWorld.py
@@ -0,0 +1,15 @@
+# FB 1st Hello World Advanced ;-;
+
+# Write a program that asks the user for their name in an input, then says hello to them by name in an output! CHECK
+
+
+# Create a project that says hello to an admin user if you or Ms. LaRose type in their name. 
+# Says hello to 5 different returning user. 
+# Says welcome for the first time for any other users.
+
+
+userName = input("What is your name? \n")
+
+pastUsersList = []
+
+print(f"Hello {userName}")

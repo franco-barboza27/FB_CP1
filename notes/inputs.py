653dd90ca2fef1a66d8380939f6476490eb4fe6a
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-20 09:42:33 -0600

    I and O notes

---
+++ notes/inputs.py
@@ -0,0 +1,28 @@
+#FB, 1st inputs outputs notes
+
+#Comments on the first line are good common practice
+#They are also useful if you want to temporarily deactivate some lines of code
+#or put placeholders
+
+#what is an input?
+#A way for the code to collect information from a user/outside source
+
+#6 What is the purpose of inputs
+#code isn't very helpful if it doesnt use new info
+
+name = input("What is your name???????????")
+
+print("GIVE ME YOU'RE NAMEEEEE!!! NOWWWW!!!!")
+nameTwo = input()
+
+#btw both methods work just dandy but stick to only one
+#  ^     ^               ^
+#Use the worfd input
+#inside the parenthesis give instructions
+
+if name == "Reg":
+    print("re-re-re-re-regulus, oh also that robot from that one anime")
+else:
+    print(f"Nice to meet you {name}")
+
+#Wow! I learned so much!

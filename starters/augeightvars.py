561504e02ebf9a6b284f9e003eae1f2ce73d263b
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-22 09:39:23 -0600

    a lot ;-; started who are you thing

---
+++ starters/augeightvars.py
@@ -0,0 +1,5 @@
+#FB 1st
+
+#num1, num2, num3, num4, result1, result2
+
+#they are wrong because they have numbers in the name and they aren't very specific for what they actually represent aside from being number placehholders
---
+++ unit_one_final/who_are_you.py
@@ -0,0 +1,9 @@
+# FB 1st Unit One Project Advanced (hopefully)
+
+# get the user's name
+# get the user's age
+# get their favorite color
+
+# save info and if the username repeats, don't ask the questions again but instead just print the finished sentence
+
+# output it in one complete sentence

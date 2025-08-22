b3745c0cc4dbd42bb6b50af184e9ce751e22a802
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-22 09:53:54 -0600

    started unit one project

--- unit_one_final/who_are_you.py
+++ unit_one_final/who_are_you.py
@@ -7,3 +7,22 @@
 # save info and if the username repeats, don't ask the questions again but instead just print the finished sentence
 
 # output it in one complete sentence
+
+print("Hello user, this is a database kind of project.")
+print("Your information will be saved until you terminate the program.")
+print("We will now begin.")
+
+
+
+def whoAreThey():
+
+    continue_forever = True
+
+    old_users = []
+
+    while continue_forever == True:
+        name = input("What is your name?\n")
+    age = input("How old are you?\n")
+    fav_color = input("What is your favorite color?\n")
+
+    final_sentence = print(f"{name} is {age} and their favorite color is {fav_color}")

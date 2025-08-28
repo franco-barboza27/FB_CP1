0cb4fc7033a56e76d6632ed4f18fb48ce218147b
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-28 09:10:37 -0600

    starter

---
+++ starters/tax.py
@@ -0,0 +1,4 @@
+# FB 1st Tax starter 8/28/25
+
+# The program simulates the cost of buying some groceries and printing that cost plus tax.
+# it's most likely because they want to change the values later or be able to plug in values at some point.

c6cec63af498c8b68a936e074716dddb0453d010
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-08-28 21:24:14 -0600

    Finished avg grade

--- projects/averageGrade.py
+++ projects/averageGrade.py
@@ -7,18 +7,22 @@
 # calculate their average grade
 # print it out into a sentence telling them their average grade
 import time as t
+import sys as s
 
 
 print("Hello, welcome to the average grade percentage calculator.")
 print("This program will calculate you average grade percent across all of your classes.")
-print("type STOP at any point to end the program.")
-start = input("Do you wish to use the calculator? Y/N:")
+print("type STOP at most points to end the program.")
+start = input("Do you wish to use the calculator? Y/N:\n")
 
 while True:
-    if start == "Y":
+
+    if start == "admin":
+        break
+    elif start == "Y":
         print("We will now begin, loading questions.")
         print("...\n\n")
-        t.sleep(2)
+        t.sleep(1)
         break
     elif start == "N" or start == "STOP":
         print("Ok, buh bye!")
@@ -29,12 +33,49 @@ while True:
 
 def calcSystem():
 
-    while True:
-        class_amount = []
+    end = False
     
+    while True:
+
+        if end == True:
+            break
+        
+        class_amount = []
+
+        grades = []
+
+        period = 0
+
         if start == "N":
             break
 
         class_num = int(input("How many classes do you have?"))
-        for num in class_num:
-            class_amount.append(num)
+        while period < class_num:
+
+            class_amount.append(period)
+            period += 1
+            question = float(input(f"What is your % grade in your period {period} class?"))
+            grades.append(question)
+        
+        grade_total = float(sum(grades))
+
+        print("Your percent grade average is ", round(grade_total/period, 3))
+
+        while True:
+
+            repeat = input("Do you wish to calculate again? Y/N:\n")
+
+            if repeat == "Y":
+                print("We will now restart, reloading questions.")
+                print("...\n\n")
+                t.sleep(1)
+                break
+            elif repeat == "N" or repeat == "STOP":
+                print("Ok, buh bye!")
+                end = True
+                break
+            else:
+                print("Im sorry but your input did not match the required format")
+
+
+calcSystem()

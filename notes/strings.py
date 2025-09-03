d4c6b1e96392519ed113b337eba3d17fb668cbf8
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-09-03 09:00:09 -0600

    stromgs motws

---
+++ notes/strings.py
@@ -0,0 +1,13 @@
+#FB 1st strings
+
+string = "There should be fifty-one characters in this string"
+
+
+print(string)
+print("The sentence above is " + str(len(string)) + " characters long.") 
+
+print(string[0:50])
+
+slicing = "word"
+
+print(slicing[0:0])

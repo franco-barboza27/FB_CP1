0c0974534fb407b054a3377e739b4608a80a34dc
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-09-10 09:50:43 -0600

    started crew shares and finished the rand notes

---
+++ notes/random_notes.py
@@ -0,0 +1,5 @@
+# FB 1st random library notes
+
+import random
+
+print(random.randint(1, 20))
---
+++ unit_two_final/crew_shares.py
@@ -0,0 +1,10 @@
+# FB 1st crew shares project
+
+# make a variable for the captain, the 1st mate and the crew mate's share amounts
+# make a variable for the total money
+# make a story
+# make a variable for the total money divided by the total shares
+# make a variable for the total money given to the captain, the first mate and the crewmates
+
+print("Once upon a time, there was a ship and it's crew...")
+tot_money = int(input("How much did they make?:\n"))

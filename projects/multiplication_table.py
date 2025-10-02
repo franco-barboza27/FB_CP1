42a2afade03cb9e5d70cb65668e496acd631c27e
franco.barboza27 <franco.barboza@ucas-edu.net>
2025-10-02 09:55:30 -0600

    Did more of the multi table

--- projects/multiplication_table.py
+++ projects/multiplication_table.py
@@ -2,10 +2,26 @@
 
 print("Hello, this is a multiplication table! If things go accordingly, you will even be able to choose how big it is!")
 
-table = []
-cur_num = 0
-size = input(print("How big do you want your multiplication table to be?"))
 
-while enumerate(table) < size:
-    cur_num += 1
-    table.append(cur_num)
+
+len_table = []
+multi_table = []
+
+table = [
+        len_table,
+        multi_table
+        ]
+
+len_size = 0
+tall_size = 0
+
+
+while True:
+    size = int(input("How big do you want your multiplication table to be?:"))
+    
+    while len(len_table) < size:
+        len_size += 1
+        len_table.append(len_size)
+
+        for mult in len_table:
+            #multi_table.append(len_size * )

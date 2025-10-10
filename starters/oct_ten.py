e16500ed052edfc2ebb5220cb5c5252c56998b70
franco.barboza27 <franco.barboza@ucas-edu.net>
2025-10-10 09:24:43 -0600

    did starter

---
+++ starters/oct_ten.py
@@ -0,0 +1,20 @@
+# FB 1st starter on October 10th
+
+import random as r
+import time as t
+
+while True:
+    
+    board  = [1, 2, 3
+              4, 5, 6
+              7, 8, 9]
+    
+
+
+    while True:
+
+        cpu = r.randint(0,9)
+
+    
+
+    

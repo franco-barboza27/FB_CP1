18039969f488cb127af08284f4381c466786fb6c
franco_barboza27 <franco.barboza@ucas-edu.net>
2025-10-24 09:54:41 -0600

    Started Simple RPG

---
+++ projects/simpleRPG.py
@@ -0,0 +1,10 @@
+# FB 1st Combat program
+
+import random
+
+char_types = [{"mage":"A magic user who relies on mental fortitude(MF) to use spells.", "Health":50, "Attack":20, "Defence":10, "Mental Energy":30, "Speed":30},
+              {"fighter":"A fighter who uses physical prowess to pummel enemies.", "Health":100, "Attack":30, "Defence":30, "Mental Energy":10, "Speed":25},
+               {"rougue":"A speedy scout with all around moderate stats", "Health":75, "Attack":20, "Defence":20, "Mental Energy":20, "Speed":40} ]
+
+mons_types = [{"Wild bat":"A large bat whose claws won't hesitate to gouge you eyes out, be careful!", "Health":40, "Attack":20, "Defence":5, "Mental Energy":0, "Speed":35},
+              {"Fire sprite":"A fist sized ball of fire with a equally petulant behavior, watch for it's potent magic!"}]

d1e4e3ddf8407823164518df7f1a339b5ada6448
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-11-04 19:24:48 -0700

    not much

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -1,18 +1,37 @@
- # FB 1st Maze generator
+# FB 1st Maze generator
 
 import turtle
 import random
 
 # make a function that makes grid the size of the maze by having a list of lists
 
+def gridsize(size):
+    rows = []
+    collumns = []
+    for i in range(1, size):
+        rows.append([])
+        collumns.append([])
+    mazemaker(rows, collumns)
 
+def is_solvable(rows, collumns):
+    pass
+
+def mazemaker(rows, collumns):
+    mazesize = len(rows)
+    everyline = 0
+    while everyline in range(0, collumns):
+        for row in rows:
+            collumns[everyline].append(1)
+            rows[everyline].append(1)
+        everyline += 1
+    
+
+    pass
+
+def drawmaze():
+    maze_maker = turtle.Turtle()
+    pass
 
 sizeask = int(input("What size of maze do you want?"))
 
-maze_maker = turtle.Turtle()
-maze_maker.hideturtle()
-maze_maker.penup()
-maze_maker.teleport(sizeask)
-
-for i in range(1, sizeask+1):
-    maze_maker.
+gridsize(sizeask)

1847318d3f821d5e91a9d0c8fe5aee0c17a177e2
franco_barboza27 <franco.barboza@ucas-edu.net>
2025-11-12 09:54:42 -0700

    Worked on order

--- projects/order.py
+++ projects/order.py
@@ -1,8 +1,16 @@
 # FB 1st restaurant menu
+# drink, main course and 2 sides and total cost
 
-# drink, main course and 2 sides
+# function to convert choices to items on the menu
+def menu_converter(choice, menudict):
+    itemnum = 1
+    choice = int(choice)
+    for i in menudict.keys():
+        if  itemnum == choice:
+            return i
+        itemnum += 1
 
-# make menus
+# make menus CHECK
       
 # so probably have 3 drinks, 3 main courses and 4 sides CHECK
 menu = [
@@ -12,13 +20,50 @@ menu = [
 ]
 
 # make introduction
-print("Welcome to Port Puddle's one and only Bug n' Grub!\n Please have a seat.")
+print("The waiter comes over.\n'Welcome to Port Puddle's one and only Bug n' Grub!'\n 'Please have a seat.' The waiter says.")
 print("You seat your carapace onto the bar stool, your mandibles twitching with hunger.")
 
 print("Would you like:")
+
+# presents each drink option
 i=1
-for drink in menu[0]:
-    print(f"{i}) {drink} for {menu[0][drink]} Rocks")
+for item in menu[0]:
+    print(f"{i}) {item} for {menu[0][item]} Rocks")
     i+=1
 print(f"{i}) None")
-drink = input("Which would you like?")
+
+#checks if it's a valid choice
+while True:
+    drinkc = input("Which would you like?1-4:\n")
+    if drinkc.isnumeric():
+        if int(drinkc) <= 0 or int(drinkc) >=5:
+            print("'Sorry I didn't quite catch that. What did you say?'")
+        else:
+            break
+    else:
+        print("'Uhhh... That's not a number on the list.'")
+
+print("'Thank you, that will come right up!'")
+
+drink = menu_converter(drinkc, menu[0])
+
+print(f"A few moments later the waiter brings you your {drink}.")
+
+print("Now then, what will your main course be?")
+# presents each main course option
+i=1
+for item in menu[0]:
+    print(f"{i}) {item} for {menu[0][item]} Rocks")
+    i+=1
+print(f"{i}) None")
+
+#checks if it's a valid choice
+while True:
+    coursec = input("Which would you like?1-4:\n")
+    if coursec.isnumeric():
+        if int(coursec) <= 0 or int(coursec) >=5:
+            print("'Sorry I didn't quite catch that. What did you say?'")
+        else:
+            break
+    else:
+        print("'Uhhh... That's not a number on the list.'")

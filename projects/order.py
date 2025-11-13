361653aa096d4b42abea30abc1c62ebd6b605dcb
franco_barboza27 <franco.barboza@ucas-edu.net>
2025-11-13 09:54:53 -0700

    Worked on order

--- projects/order.py
+++ projects/order.py
@@ -12,12 +12,14 @@ def menu_converter(choice, menudict):
 
 # make menus CHECK
       
-# so probably have 3 drinks, 3 main courses and 4 sides CHECK
+# so probably have 4 drinks, 4 main courses and 4 sides CHECK
 menu = [
-    {"Honey suckle juice":2.25, "Leaf water":1.00, "SodAphid":5.50},
-    {"Hot Fly Grub":10.00, "Port Puddle Specialty Sliders":86.87, "Mosquito brine Soup":12.00},
-    {"Acorn Leaf Salad":1.00, "Dirt Bowl":1.00, "Larva Slices":2.00, "Port Puddle Specialty Grilly":50.49}
+    {"Honey suckle juice":2.25, "Leaf water":1.00, "SodAphid":5.50, "Choccy Milkweed Milk":2.50},
+    {"Hot Fly Grub":10.00, "Port Puddle Specialty Sliders":86.87, "Mosquito brine Soup":12.00, "Tree Bark Ribs":20.00},
+    {"Acorn Leaf Salad":1.00, "Dirt Bowl":1.00, "Larva Slices":2.00, "Port Puddle Specialty Grilly":50.49, "Fried Shells":3.00}
 ]
+
+order = []
 
 # make introduction
 print("The waiter comes over.\n'Welcome to Port Puddle's one and only Bug n' Grub!'\n 'Please have a seat.' The waiter says.")
@@ -28,42 +30,74 @@ print("Would you like:")
 # presents each drink option
 i=1
 for item in menu[0]:
-    print(f"{i}) {item} for {menu[0][item]} Rocks")
+    print(f"{i}) {item} for {menu[0][item]:.2f} Rocks")
     i+=1
 print(f"{i}) None")
 
-#checks if it's a valid choice
+# checks if it's a valid choice for the drink
 while True:
     drinkc = input("Which would you like?1-4:\n")
     if drinkc.isnumeric():
         if int(drinkc) <= 0 or int(drinkc) >=5:
-            print("'Sorry I didn't quite catch that. What did you say?'")
+            print("'Sorry I didn't quite catch that. What did you say?'(maybe try a number in the list I JUST gave you.)")
         else:
             break
     else:
-        print("'Uhhh... That's not a number on the list.'")
+        print("'Uhhh... That's not a number... could you repeat that?'")
 
-print("'Thank you, that will come right up!'")
+# make sure theres a safe guard for when the choose "None"
 
 drink = menu_converter(drinkc, menu[0])
+if drink is not "None":
+    print("'Thank you, that will come right up!'")
+    order.append(drink)
+    print(f"A few moments later the waiter brings you your {drink}.")
 
-print(f"A few moments later the waiter brings you your {drink}.")
 
 print("Now then, what will your main course be?")
 # presents each main course option
 i=1
-for item in menu[0]:
-    print(f"{i}) {item} for {menu[0][item]} Rocks")
+for item in menu[1]:
+    print(f"{i}) {item} for {menu[1][item]:.2f} Rocks")
     i+=1
 print(f"{i}) None")
 
-#checks if it's a valid choice
+# checks if it's a valid choice for the course
 while True:
     coursec = input("Which would you like?1-4:\n")
     if coursec.isnumeric():
         if int(coursec) <= 0 or int(coursec) >=5:
-            print("'Sorry I didn't quite catch that. What did you say?'")
+            print("'Sorry I didn't quite catch that. What did you say?'\n(maybe try a number in the list I JUST gave you.)")
         else:
             break
     else:
-        print("'Uhhh... That's not a number on the list.'")
+        print("'Uhhh... That's not a number... could you repeat that?'")
+
+print("'Thank you, that will come right up!'")
+mcourse = menu_converter(coursec, menu[1])
+order.append(mcourse)
+    
+# present the sides and do it twice
+sidenum = 1
+print("You may choose two sides along with that.")
+while sidenum in range(1,3):
+    i=1
+    for item in menu[2]:
+        print(f"{i}) {item} for {menu[2][item]:.2f} Rocks")
+        i+=1
+
+    # checks if it's a valid choice for the course
+    while True:
+        sidec = input("Which would you like?1-4:\n")
+        if sidec.isnumeric():
+            if int(sidec) <= 0 or int(sidec) >=5:
+                print("'Sorry I didn't quite catch that. What did you say?'\n(maybe try a number in the list I JUST gave you.)")
+            else:
+                break
+        else:
+            print("'Uhhh... That's not a number... could you repeat that?'")
+    sidenum += 1
+# when checking the price,
+    # if price is zero be like "get out of my restaraunt!"
+    # otherwise be fine and give them their total
+        # also dont even print anything if they order "None"

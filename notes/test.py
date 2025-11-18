d2c57548f4eccb36bd1907ed03cf21ebf4bd10f0
franco-barboza27 <franco.barboza@ucas-edu.net>
2025-11-18 09:17:04 -0700

    Testing connection

--- notes/test.py
+++ notes/test.py
@@ -1,5 +1,5 @@
 # FB 1st codespace stuff
-
+import time as t
 print("test")
 
 print("another test")
@@ -11,3 +11,11 @@ print(var)
 print("Test x3")
 
 # I love using this file for like, everything
+
+# bleep boop bap
+
+print("test")
+t.sleep(1)
+print("does this work and save and push??")
+t.sleep(0.5)
+print("I hope so!")
